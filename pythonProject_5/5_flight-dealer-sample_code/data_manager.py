dcf454b3b1fe7e33b37781490df0da80e47b6124
wentung <wentungwen@gmail.com>
2023-04-11 09:37:57 +0200

    feat: flight dealer's teacher sample code (https://replit.com/@wentung/flight-deals-end#flight_search.py)

---
+++ pythonProject_5/5_flight-dealer-sample_code/data_manager.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+import requests
+
+SHEETY_PRICES_ENDPOINT = YOUR SHEETY PRICES ENDPOINT
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        """ get location data from sheety and return a dict"""
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        """ write the IATA data to sheety"""
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
