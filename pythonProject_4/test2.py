c290420e59d78f0304064c4e657522b7b688e339
wentung <wentungwen@gmail.com>
2023-04-19 14:21:56 +0200

    re-organize the project directory

--- pythonProject_4/test2.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-master = Tk()
-
-w = Canvas(master, width=250, height=200)
-w.create_rectangle(0, 0, 100, 100, fill="blue", outline = 'blue')
-w.create_rectangle(50, 50, 100, 100, fill="red", outline = 'blue') 
-w.pack()
-master.mainloop()
