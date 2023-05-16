53a6451cec00fb79df8d6c6f4f75ae2948d394bc
wentung <wentungwen@gmail.com>
2023-05-16 17:12:50 +0200

    Feat: blog done with 3 sub-pages and individual posts

--- 7_flask_projects/7_8_miniblog/main.py
+++ 7_flask_projects/7_8_miniblog/main.py
@@ -1,20 +1,48 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", title="Wen's main page", subtitle="Welcome to Wen's secret garden!")
+    title = "Wen's main page"
+    bg_url = "../static/assets/img/home-bg.jpg"
+    subtitle = "Welcome to Wen's secret garden!"
+    posts = requests.get("https://api.npoint.io/38e9b069f69709e85dc6").json()
+    return render_template("index.html",
+                           title=title,
+                           subtitle=subtitle,
+                           bg_url=bg_url,
+                           posts=posts)
+
+
+@app.route('/contact_receive', methods=["POST"])
+def receive_data():
+    print(123)
 
 
 @app.route('/about')
 def about():
-    return render_template("about.html", title="About Wen", subtitle="I'm a happy engineer and artist.")
+    title = "About Wen"
+    subtitle = "I'm a happy engineer and artist."
+    bg_url = "../static/assets/img/about-bg.jpg"
+    return render_template("about.html", title=title, subtitle=subtitle, bg_url=bg_url)
+
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html", title="Contact", subtitle="Feel free to contact me!")
+    title = "Contact"
+    subtitle = "Feel free to contact me!"
+    bg_url = "../static/assets/img/contact-bg.jpg"
+    return render_template("contact.html", title=title, subtitle=subtitle, bg_url=bg_url)
+
+
+@app.route('/post/<int:post_id>')
+def blog_post(post_id):
+    post = requests.get("https://api.npoint.io/38e9b069f69709e85dc6").json()[post_id-1]
+    return render_template("post.html", post=post)
+
 
 
 if __name__ == "__main__":
