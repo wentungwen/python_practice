e980f43af19307eba96aa8b41ec0a48690b69671
wentung <wentungwen@gmail.com>
2023-05-23 10:22:09 +0200

    Feat: refactor to connect to database

--- 8_SQL_Flask_projects/8_2_movie_library_SQLAlchemy/main.py
+++ 8_SQL_Flask_projects/8_2_movie_library_SQLAlchemy/main.py
@@ -1,44 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+db.init_app(app)
+
 Bootstrap(app)
 
-all_books = [
-    {
-        "title": "nice book",
-        "author": "somebody",
-        "rating": 3
-    }
-]
+all_books = []
 
 
-def get_db_connection():
-    conn = sqlite3.connect('books-collection.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=True, unique=True)
+    author = db.Column(db.String(250), nullable=True)
+    rating = db.Column(db.Float, nullable=True)
+
+
+def check_rating_type(rating):
+    try:
+        float(rating)
+        return True
+    except ValueError:
+        return False
 
 
 @app.route('/')
 def home():
-    books = all_books
+    books = Book.query.all()
     return render_template("index.html", books=books)
-
-
-# init the table
-try:
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS books ("
-                   "id INTEGER PRIMARY KEY, "
-                   "title varchar(250) NOT NULL UNIQUE, "
-                   "author varchar(250) NOT NULL, "
-                   "rating FLOAT NOT NULL)")
-    cursor.close()
-    conn.close()
-except sqlite3.Error as err:
-    print("Error creating table:", err)
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -48,21 +41,40 @@ def add():
         author = request.form.get('author')
         rating = request.form.get('rating')
 
-        # update the table
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        sql = "INSERT INTO books (title, author, rating) VALUES (?, ?, ?)"
-        cursor.execute(sql, (title, author, rating))
-        cursor.execute(f"INSERT INTO books VALUES(1, '{title}', '{author}', '{rating}')")
-        conn.commit()
-
-        cursor.close()
-        conn.close()
-
-        return render_template('success.html')
+        if check_rating_type(rating):
+            book = Book(title=title, author=author, rating=rating)
+            db.session.add(book)
+            db.session.commit()
+            return render_template("success.html")
+        else:
+            return render_template('error.html', message="wrong type!")
     else:
         return render_template('add.html')
+
+
+@app.route('/edit/<int:book_id>', methods=["POST", "GET"])
+def edit(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return render_template("error.html", message="Book not found.")
+
+    if request.method == "POST":
+        new_rating = request.form.get('rating')
+        if check_rating_type(new_rating):
+            book.rating = new_rating
+            db.session.commit()
+            return render_template("success.html")
+        else:
+            return render_template('error.html', message="Wrong type!")
+    return render_template("edit.html", book=book)
+
+
+@app.route('/delete/<int:book_id>')
+def delete(book_id):
+    book = Book.query.get(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 @app.route('/success')
@@ -70,5 +82,12 @@ def success():
     return render_template("success.html")
 
 
+@app.route('/error')
+def error():
+    return render_template("error.html")
+
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
