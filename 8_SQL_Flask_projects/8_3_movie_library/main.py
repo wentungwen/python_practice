6caaa4a4986ae520a49a83050f285948ba4fc5d8
wentung <wentungwen@gmail.com>
2023-07-03 14:32:53 +0200

    delete sqlalchemy.exc

--- 8_SQL_Flask_projects/8_3_movie_library/main.py
+++ 8_SQL_Flask_projects/8_3_movie_library/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import OperationalError
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
